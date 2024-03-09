e3efeedd0d806aa0373bd9c258b47bfa0b3e799d
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-03-09 05:20:43 +0100

    Expand on ADDR defs and sysregs
    Addr defs are used as general place holders for memory locations that we
    define from the linker.
    
    Also define some sysregs and offsets we neeed.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

---
+++ include/kernel/addr_defs.h
@@ -0,0 +1,16 @@
+#ifndef __ADDR_DEFS_H
+#define __ADDR_DEFS_H
+
+extern void __start();
+extern void __end();
+extern void __data_start();
+extern void __data_end();
+extern void __text_start();
+extern void __stackpage_start();
+extern void __stackpage_end();
+extern void __earlypage_start();
+extern void __earlypage_end();
+
+extern void _start();
+
+#endif
