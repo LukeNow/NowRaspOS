0341f56a9651adb27b2ed0650131a4f4e8ec3e15
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-04-25 18:47:19 -0700

    common/math.h: Add first math functions that are useful
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

---
+++ include/common/math.h
@@ -0,0 +1,12 @@
+#ifndef __MATH_H
+#define __MATH_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+unsigned int math_log2_64(uint64_t num);
+unsigned int math_is_power2_64(uint64_t num);
+
+#define MATH_LOG2(X, RET) do { RET=0; while (num > 1) { num >>= 1; ret++; }} while(0)
+
+#endif
---
+++ src/common/math.c
@@ -0,0 +1,44 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <common/math.h>
+#include <kernel/printf.h>
+#include <kernel/uart.h>
+#include <common/assert.h>
+
+// NUM cannot be 0
+unsigned int math_log2_64(uint64_t num)
+{
+    ASSERT(num);
+    int ret = 0;
+
+    while (num > 1) {
+        num >>= 1;
+        ret++;
+    }
+
+    return ret;
+}
+
+// 1 for true, 0 for false. Can be confusing when posix definitions for errors are non zero :)
+unsigned int math_is_power2_64(uint64_t num)
+{
+    uint64_t mask = 1;
+
+    for (int i = 0; i < 64; i++) {
+        // We found a 2^n bit
+        if (num & mask) {
+            // There are other digits so this cannot be power of 2
+            if (num & ~mask)
+                return 0;
+            // There are no other digits so this is a power of 2
+            else
+                return 1;
+        }
+
+        mask <<= 1;
+    }   
+
+    // We shouldnt reach here
+    ASSERT(0);
+    return 1;
+}
