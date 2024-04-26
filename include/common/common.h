1092a1c874bf8dd78031ceb0b52b8c149c575a5c
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-04-26 02:20:45 -0700

    common.h: Add Round_up and other useful defines
    Taken from previous project.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/common/common.h
+++ include/common/common.h
@@ -17,5 +17,10 @@ typedef uint32_t flags_t;
 #define BIT_ALIGN(X, Y) ((X) & BITS(Y))
 #define IS_ALIGNED(X, Y) ((X) % (Y) ? 0 : 1)
 #define IS_BIT_ALIGNED(X,Y) (BIT_ALIGN(X, Y) ? 0 : 1)
+#define ROUND_DOWN(x, size) ((x) - ((x) % (size)))
+#define ROUND_UP(x, size) IS_ALIGNED((x), (size)) ? \
+		(x) : ((x) + ((size) - ((x) % (size)))) 
+#define ALIGN_DOWN(x, size) ROUND_DOWN((x) + (size) - 1, (size))
+#define ALIGN_UP(x, size) ROUND_UP((x), (size))
 
 #endif
