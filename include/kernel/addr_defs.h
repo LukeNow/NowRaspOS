68bc37d26f9354aba5ed983d7f10708cb334e164
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-06-14 01:08:07 -0700

    include/addr_defs.h Add addtional labels as defs
    Add them so the addresses of memory sections from the linker can be
    accessed.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/kernel/addr_defs.h
+++ include/kernel/addr_defs.h
@@ -5,11 +5,17 @@ extern void __start();
 extern void __end();
 extern void __data_start();
 extern void __data_end();
+extern void __rodata_start();
+extern void __rodata_end();
 extern void __text_start();
 extern void __stackpage_start();
 extern void __stackpage_end();
 extern void __earlypage_start();
 extern void __earlypage_end();
+extern void __bss_start();
+extern void __bss_end();
+extern void __early_page_start();
+extern void __early_page_end();
 
 extern void _start();
 
