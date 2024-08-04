6f252e5ab5d6acad6a3297b4141e3922e400ddab
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-08-04 12:56:28 -0700

    common/bits: Add bits file for common bit operations
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/common/bits.h
+++ include/common/bits.h
@@ -13,7 +13,17 @@ typedef uint64_t bits_t;
 #define BIT_ALIGN(X, Y) ((X) & BITS(Y))
 #define IS_BIT_ALIGNED(X,Y) (BIT_ALIGN(X, Y) ? 0 : 1)
 
+unsigned int bits_msb_index_64(uint64_t bits);
+unsigned int bits_msb_index_32(uint32_t bits);
 unsigned int bits_count_64(uint64_t bits);
 unsigned int bits_count_32(uint32_t bits);
+void bits_set_64(uint64_t * bits, unsigned int index, unsigned int num_bits);
+void bits_set_32(uint32_t * bits, unsigned int index, unsigned int num_bits);
+uint64_t bits_get_64(uint64_t * bits, unsigned int index, unsigned int num_bits);
+uint32_t bits_get_32(uint32_t * bits, unsigned int index, unsigned int num_bits);
+void bits_free_64(uint64_t * bits, unsigned int index, unsigned int num_bits);
+void bits_free_32(uint32_t * bits, unsigned int index, unsigned int num_bits);
+void bits_flip_64(uint64_t * bits, unsigned int index, unsigned int num_bits);
+void bits_flip_32(uint32_t * bits, unsigned int index, unsigned int num_bits);
 
 #endif
--- src/common/bits.c
+++ src/common/bits.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <common/common.h>
 #include <common/bits.h>
+#include <common/assert.h>
 
 unsigned int _bits_count(uint64_t bits, unsigned int num)
 {   
@@ -16,6 +17,32 @@ unsigned int _bits_count(uint64_t bits, unsigned int num)
     return count;
 }
 
+unsigned int bits_msb_index_64(uint64_t bits)
+{
+    uint64_t mask = 1;
+
+    for (unsigned int i = 0; i < 63; i++) {
+        if (bits < mask)
+            return i;
+        mask = mask << 1;
+    }
+
+    return 63;
+}
+
+unsigned int bits_msb_index_32(uint32_t bits)
+{
+    uint64_t mask = 1;
+
+    for (unsigned int i = 0; i < 31; i++) {
+        if (bits < mask)
+            return i;
+        mask = mask << 1;
+    }
+
+    return 31;
+}
+
 unsigned int bits_count_64(uint64_t bits)
 {
     return _bits_count(bits, 64);
@@ -24,4 +51,44 @@ unsigned int bits_count_64(uint64_t bits)
 unsigned int bits_count_32(uint32_t bits)
 {
     return _bits_count(bits, 32);
+}
+
+void bits_set_64(uint64_t * bits, unsigned int index, unsigned int num_bits)
+{
+    *bits = *bits | ((uint64_t)BITS(num_bits) << index);
+}
+
+void bits_set_32(uint32_t * bits, unsigned int index, unsigned int num_bits)
+{
+    *bits = *bits | ((uint32_t)BITS(num_bits) << index);
+}
+
+uint64_t bits_get_64(uint64_t * bits, unsigned int index, unsigned int num_bits)
+{
+    return (uint64_t)(*bits & ((uint64_t)BITS(num_bits) << index)) >> index;
+}
+
+uint32_t bits_get_32(uint32_t * bits, unsigned int index, unsigned int num_bits)
+{
+    return (uint32_t)(*bits & ((uint32_t)BITS(num_bits) << index)) >> index;
+}
+
+void bits_free_64(uint64_t * bits, unsigned int index, unsigned int num_bits)
+{
+    *bits = (*bits &~ ((uint64_t)BITS(num_bits) << index));
+}
+
+void bits_free_32(uint32_t * bits, unsigned int index, unsigned int num_bits)
+{
+    *bits = (*bits &~ ((uint32_t)BITS(num_bits) << index));
+}
+
+void bits_flip_64(uint64_t * bits, unsigned int index, unsigned int num_bits)
+{
+    *bits = (*bits ^ ((uint64_t)BITS(num_bits) << index));
+}
+
+void bits_flip_32(uint32_t * bits, unsigned int index, unsigned int num_bits)
+{
+    *bits = (*bits ^ ((uint32_t)BITS(num_bits) << index));
 }
