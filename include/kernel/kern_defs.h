36fa8cb29b4986e8cc2aa39a72f0d7c97734bf75
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-08-04 12:59:13 -0700

    kernel/kern_defs.h: Add more PAGE definitions
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/kernel/kern_defs.h
+++ include/kernel/kern_defs.h
@@ -5,7 +5,10 @@
 #include <common/common.h>
 
 #define PAGE_SIZE 4096
+#define PAGE_MASK ~(PAGE_SIZE - 1)
 #define PAGE_BIT_OFF 12
 #define UPPER_ADDR_MASK 0xFFFF000000000000
+
+#define PAGE_INDEX_FROM_PTR(PTR) ((uint64_t)(PTR) / PAGE_SIZE)
 
 #endif
