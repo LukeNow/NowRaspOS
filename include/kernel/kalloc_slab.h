a5b1e4a86b42520c397b810b4520ec0279d71712
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-08-04 13:08:32 -0700

    kernel/kalloc_slab: Implement kalloc_slab
    Implement a data structure for oraganizing and allocating
    memory in a continuous page range.
    
    Very similiar to Linux's slab implementation and to bufctl.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

---
+++ include/kernel/kalloc_slab.h
@@ -0,0 +1,48 @@
+#ifndef __KALLOC_SLAB_H
+#define __KALLOC_SLAB_H
+
+#include <common/common.h>
+#include <common/linkedlist.h>
+#include <kernel/kern_defs.h>
+
+/* kalloc_slab flags */
+/* This slab was externally allocated outside of the page allocator or should not be freed until done explicitly. */
+#define KALLOC_SLAB_EXTERN_ALLOC_F (1 << 1)
+
+/* Pointer to the free index list, which should be directly after the struct in memory. */
+#define KALLOC_SLAB_FREE_INDEXS(slab) ((kalloc_slab_t*)(slab) + 1)
+/* Macro to extract the current cache list this slab belongs to, or any data we store in the ll node. */
+#define KALLOC_SLAB_CURR_LIST_P(slab) ((ll_node_t *)((slab)->node.data))
+
+#define KALLOC_SLAB_VERIFY(slab) ASSERT_PANIC(slab, "Slab ptr is null"); \
+                        ASSERT_PANIC(((uint64_t)slab->mem_ptr & PAGE_MASK) == ((uint64_t)slab  & PAGE_MASK), "Slab memptr is not in slab ptr range"); \
+                        ASSERT_PANIC(slab->obj_size, "Slab obj size nonzer0");
+
+/* Very similar to the slab alloc linux uses. With the free indexs being the buf_ctl scheme linux also uses for updating
+ * and getting free_indexs. */
+typedef struct kalloc_slab {
+    unsigned int max_num;
+    unsigned int num;
+    size_t obj_size;
+    void * mem_ptr;
+    ll_node_t node;
+    flags_t flags;
+    unsigned int mem_page_num;
+    unsigned int free_index;
+    unsigned int * free_indexs;
+} kalloc_slab_t __attribute__ ((aligned (8)));
+
+/* Size of the struct for the desired object number. */
+size_t kalloc_slab_struct_size(unsigned int obj_num, size_t obj_size);
+/* Size of the memory taken by the slab struct and the objects alloced after it. */
+size_t kalloc_slab_total_size(unsigned int obj_num, size_t obj_size);
+/* The amount of objects that fit into a given mem_size, not taking into account inline structs. */
+unsigned int kalloc_slab_obj_num(size_t obj_size, size_t mem_size);
+/* The number of objs in the slab that can fit alongside the inlined slab struct. */
+unsigned int kalloc_slab_inline_obj_num_pages(size_t obj_size, unsigned int page_num);
+/* Return either the passed in slab object to be initialized or the slab object initialized in the mem_ptr mem region. */
+kalloc_slab_t * kalloc_slab_init(kalloc_slab_t * slab, void * mem_ptr, unsigned int mem_num_pages, size_t obj_size, flags_t flags);
+void * kalloc_slab_alloc(kalloc_slab_t * slab);
+int kalloc_slab_free(kalloc_slab_t * slab, void * obj);
+
+#endif
---
+++ src/kernel/kalloc_slab.c
@@ -0,0 +1,137 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <common/common.h>
+#include <common/assert.h>
+#include <common/string.h>
+#include <kernel/kalloc_slab.h>
+#include <kernel/kern_defs.h>
+
+size_t kalloc_slab_free_list_size(unsigned int obj_num)
+{
+    return (sizeof(unsigned int) * (obj_num - 1));
+}
+
+// return the size of the slab struct for allocating 
+size_t kalloc_slab_struct_size(unsigned int obj_num, size_t obj_size)
+{
+    // The struct with the free_indexs array included after the struct
+    // The free indexs array is size (objnum - 1)
+    // Align everything up to the obj size so that objs start on their own size byte boundary
+    return (size_t)ALIGN_UP(sizeof(kalloc_slab_t) + kalloc_slab_free_list_size(obj_num), obj_size);
+}
+
+// return the size of the total mem size when in-lining the struct in the memory region
+size_t kalloc_slab_total_size(unsigned int obj_num, size_t obj_size)
+{
+    return (size_t)(ALIGN_UP(kalloc_slab_struct_size(obj_num, obj_size), obj_size) + (obj_num * obj_size));
+}
+
+unsigned int kalloc_slab_obj_num(size_t obj_size, size_t mem_size)
+{
+    return ALIGN_DOWN(mem_size, obj_size) / obj_size;
+}
+
+// TODO make this more logarithmic or make better guesses
+// return the number of objs that can fit within a page alongside the embedded slab struct
+unsigned int kalloc_slab_inline_obj_num_pages(size_t obj_size, unsigned int num_pages)
+{
+    size_t total_size;
+    size_t mem_size = num_pages * PAGE_SIZE;
+
+    // Start the best guess at the number of objects with the struct and free list with 1 object
+    // TODO we could make this much more efficient
+    unsigned int num = kalloc_slab_obj_num(obj_size, mem_size);
+    do {
+        total_size = kalloc_slab_total_size(num, obj_size);
+        num --;
+    } while (num > 0 && total_size > mem_size);
+
+    return num;
+}
+
+kalloc_slab_t * kalloc_slab_init(kalloc_slab_t * slab, void * mem_ptr, unsigned int mem_num_pages, size_t obj_size, flags_t flags)
+{
+    size_t struct_size;
+    unsigned int num;
+    
+    ASSERT(obj_size % sizeof(uint32_t) == 0 && mem_num_pages);
+
+    // We are allocating the struct embedded in the mem_region provided
+    if (!slab) {
+        memset(mem_ptr, 0, mem_num_pages * PAGE_SIZE);
+
+        num = kalloc_slab_inline_obj_num_pages(obj_size, mem_num_pages);
+
+        ASSERT_PANIC(num, "Calculated num of objects is 0");
+
+        struct_size = kalloc_slab_struct_size(num, obj_size);
+
+        slab = (kalloc_slab_t *)mem_ptr;
+        slab->mem_ptr = (char*)mem_ptr + struct_size;
+
+        ASSERT_PANIC((((uint64_t)slab->mem_ptr) + (slab->obj_size * slab->max_num)) < ((uint64_t)slab + PAGE_SIZE * mem_num_pages), 
+                    "Slab mem out of range");
+    // We are allocating from a struct provided and filling the mem_ptr region up to the max_num objects providied
+    } else {
+        num = kalloc_slab_obj_num(obj_size, PAGE_SIZE * mem_num_pages);
+        // zero the struct provided
+        memset(slab, 0, kalloc_slab_struct_size(num, obj_size));
+        // Zero the object area
+        memset(mem_ptr, 0, num * obj_size);
+
+        slab->mem_ptr = mem_ptr;
+    }
+
+    slab->free_indexs = KALLOC_SLAB_FREE_INDEXS(slab);
+    slab->free_index = 0;
+    for (int i = 0; i < num - 1; i++) {
+        // For every index the next index is free
+        slab->free_indexs[i] = i + 1;
+    }
+
+    /* We are reserving one memory space for the overflow that occurs when we fully allocate 
+     * the slab, so that slab->free_index is always pointing to an actual free index. */
+    slab->max_num = num - 1;
+    slab->obj_size = obj_size;
+    slab->mem_page_num = mem_num_pages;
+
+    // This is the first free index
+    
+    slab->flags = flags;
+
+    // either return the given slab or return the slab inlined in the mem_ptr region
+    return slab;
+}
+
+void * kalloc_slab_alloc(kalloc_slab_t * slab)
+{   
+    ASSERT_PANIC(slab->num != slab->max_num, "Slab is already full");
+    ASSERT_PANIC(slab->free_index <= slab->max_num, "Free index is not in max num range");
+    ASSERT_PANIC(slab->free_indexs[slab->free_index] <= slab->max_num, "Free indexs are not in max num range");
+    
+    void * obj = slab->mem_ptr + slab->obj_size * slab->free_index;
+    slab->free_index = slab->free_indexs[slab->free_index];
+    slab->num++;
+
+    ASSERT_PANIC(PTR_IN_RANGE(obj, slab->mem_ptr, slab->obj_size * (slab->max_num  + 1)), 
+                "Alloced obj outside of mem range");
+    return obj;
+}
+
+int kalloc_slab_free(kalloc_slab_t * slab, void * obj)
+{   
+    ASSERT_PANIC(slab->num, "Slab is all freed");
+
+    ASSERT_PANIC(PTR_IN_RANGE(obj, slab->mem_ptr, slab->obj_size * (slab->max_num  + 1)), 
+                "Obj outside of slab range.");
+
+    unsigned int index = (obj - slab->mem_ptr) / slab->obj_size;
+
+    ASSERT_PANIC(index <= slab->max_num, "index of free greated than max num entries");
+    slab->free_indexs[index] = slab->free_index;
+    slab->free_index = index;
+    slab->num --;
+
+    return 0;
+}
+
