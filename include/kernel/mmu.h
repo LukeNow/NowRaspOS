81da43fac249e6a0cdc1acdf5e1e9d8907b18872
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-09-18 10:00:55 -0700

    kernel/mmu.h: Implement initial paging
    Implement the initial paging by first mapping the ram space to the lower
    address space. Next we will implement full paging and move to the kernel
    address space where we can properly implement paging and mapping pages
    for virtual addresses.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/kernel/mmu.h
+++ include/kernel/mmu.h
@@ -1,6 +1,106 @@
 #ifndef __MMU_H
 #define __MMU_H
 
-int mmu_init();
+#include <stdint.h>
+#include <stddef.h>
+#include <common/bits.h>
+
+#define MMU_VA_SIZE_BITS 39
+#define MMU_UPPER_ADDRESS (BITS(64 - MMU_VA_SIZE_BITS) << MMU_VA_SIZE_BITS)
+
+#define MT_DEVICE_NGNRNE	0
+#define MT_DEVICE_NGNRE		1
+#define MT_DEVICE_GRE		2
+#define MT_NORMAL_NC		3
+#define MT_NORMAL		    4
+
+#define PAGE_OFF 12
+#define PAGE_MASK (PAGE_SIZE - 1)
+
+#define PT_ADDR (BITS(36) << 12)
+#define PT_SET_ADDR(ADDR) (((ADDR) & PAGE_MASK) << 12)
+
+#define PT_ENTRY    (3 << 0)
+#define PT_PAGE     (3 << 0)     
+#define PT_BLOCK    (1 << 0)
+#define PT_MEM_ATTR(ATTR) ((ATTR) << 2)
+#define PT_NO_WRITE_EL0 (2 << 6)
+#define PT_NO_READ_EL0 (1 << 6)
+#define PT_OUTER_SHAREABLE (2 << 8)
+#define PT_INNER_SHAREABLE (3 << 8)
+#define PT_ACCESSED (1 << 10)
+#define PT_NO_EXECUTE (1 << 54)
+#define PT_NO_EXECUTE_LOWER_EL (1 << 59)
+#define PT_NO_TRANSLATE_LOWER_EL (1 << 60)
+#define PT_SECURE (1 << 63)
+
+#define MAIR1_VAL ((0x00ul << (MT_DEVICE_NGNRNE * 8)) |\
+                 (0x04ul << (MT_DEVICE_NGNRE * 8)) |\
+				 (0x0cul << (MT_DEVICE_GRE * 8)) |\
+                 (0x44ul << (MT_NORMAL_NC * 8)) |\
+				 (0xfful << (MT_NORMAL * 8)) )
+
+#define SCTLREL1_VAL ( (0xC00800) |		/* set mandatory reserved bits */\
+					  (1 << 12)  |      /* I, Instruction cache enable. This is an enable bit for instruction caches at EL0 and EL1 */\
+					  (1 << 4)   |		/* SA0, tack Alignment Check Enable for EL0 */\
+					  (1 << 3)   |		/* SA, Stack Alignment Check Enable */\
+					  (1 << 2)   |		/* C, Data cache enable. This is an enable bit for data caches at EL0 and EL1 */\
+					  (1 << 1)   |		/* A, Alignment check enable bit */\
+					  (1 << 0) )		/* set M, enable MMU */
+
+#define TCREL1_VAL  ( (0b00LL << 37) |   /* TBI=0, no tagging */\
+					 (0b001LL << 32) |  /* IPS= 32 bit ... 000 = 32bit, 001 = 36bit, 010 = 40bit */\
+					 (0b10LL << 30)  |  /* TG1=4k ... options are 10=4KB, 01=16KB, 11=64KB ... take care differs from TG0 */\
+					 (0b11LL << 28)  |  /* SH1=3 inner ... options 00 = Non-shareable, 01 = INVALID, 10 = Outer Shareable, 11 = Inner Shareable */\
+					 (0b01LL << 26)  |  /* ORGN1=1 write back .. options 00 = Non-cacheable, 01 = Write back cacheable, 10 = Write thru cacheable, 11 = Write Back Non-cacheable */\
+					 (0b01LL << 24)  |  /* IRGN1=1 write back .. options 00 = Non-cacheable, 01 = Write back cacheable, 10 = Write thru cacheable, 11 = Write Back Non-cacheable */\
+					 (0b0LL  << 23)  |  /* EPD1 ... Translation table walk disable for translations using TTBR1_EL1  0 = walk, 1 = generate fault */\
+					 (25LL   << 16)  |  /* T1SZ=25 (512G) ... The region size is 2 POWER (64-T1SZ) bytes */\
+					 (0b00LL << 14)  |  /* TG0=4k  ... options are 00=4KB, 01=64KB, 10=16KB,  ... take care differs from TG1 */\
+					 (0b11LL << 12)  |  /* SH0=3 inner ... .. options 00 = Non-shareable, 01 = INVALID, 10 = Outer Shareable, 11 = Inner Shareable */\
+					 (0b01LL << 10)  |  /* ORGN0=1 write back .. options 00 = Non-cacheable, 01 = Write back cacheable, 10 = Write thru cacheable, 11 = Write Back Non-cacheable */\
+					 (0b01LL << 8)   |  /* IRGN0=1 write back .. options 00 = Non-cacheable, 01 = Write back cacheable, 10 = Write thru cacheable, 11 = Write Back Non-cacheable */\
+					 (0b0LL  << 7)   |  /* EPD0  ... Translation table walk disable for translations using TTBR0_EL1  0 = walk, 1 = generate fault */\
+					 (25LL   << 0) ) 	/* T0SZ=25 (512G)  ... The region size is 2 POWER (64-T0SZ) bytes */
+
+
+typedef union {
+	struct {
+		uint64_t EntryType : 2;				// @0-1		1 for a block table, 3 for a page table
+		    
+			/* These are only valid on BLOCK DESCRIPTOR */
+			uint64_t MemAttr : 4;			// @2-5
+			enum {
+				STAGE2_S2AP_NOREAD_EL0 = 1,	//			No read access for EL0
+				STAGE2_S2AP_NO_WRITE = 2,	//			No write access
+			} S2AP : 2;						// @6-7
+			enum {
+				STAGE2_SH_OUTER_SHAREABLE = 2,	//			Outter shareable
+				STAGE2_SH_INNER_SHAREABLE = 3,	//			Inner shareable
+			} SH : 2;						// @8-9
+			uint64_t AF : 1;				// @10		Accessable flag
+
+		uint64_t _reserved11 : 1;			// @11		Set to 0
+		uint64_t Address : 36;				// @12-47	36 Bits of address
+		uint64_t _reserved48_51 : 4;		// @48-51	Set to 0
+		uint64_t Contiguous : 1;			// @52		Contiguous
+		uint64_t _reserved53 : 1;			// @53		Set to 0
+		uint64_t XN : 1;					// @54		No execute if bit set
+		uint64_t _reserved55_58 : 4;		// @55-58	Set to 0
+		
+		uint64_t PXNTable : 1;				// @59      Never allow execution from a lower EL level 
+		uint64_t XNTable : 1;				// @60		Never allow translation from a lower EL level
+		enum {
+			APTABLE_NOEFFECT = 0,			// No effect
+			APTABLE_NO_EL0 = 1,				// Access at EL0 not permitted, regardless of permissions in subsequent levels of lookup
+			APTABLE_NO_WRITE = 2,			// Write access not permitted, at any Exception level, regardless of permissions in subsequent levels of lookup
+			APTABLE_NO_WRITE_EL0_READ = 3	// Write access not permitted,at any Exception level, Read access not permitted at EL0.
+		} APTable : 2;						// @61-62	AP Table control .. see enumerate options
+		uint64_t NSTable : 1;				// @63		Secure state, for accesses from Non-secure state this bit is RES0 and is ignored
+	};
+	uint64_t Raw64;							// @0-63	Raw access to all 64 bits via this union
+} VMSAv8_64_DESCRIPTOR;
+
+int mmu_init(uint32_t phy_mem_size, uint32_t vc_mem_start, uint32_t vc_mem_size);
 
 #endif
--- src/kernel/mmu.c
+++ src/kernel/mmu.c
@@ -7,352 +7,173 @@
 #include <kernel/mm.h>
 #include <kernel/gpio.h>
 #include <common/bits.h>
+#include <kernel/aarch64_common.h>
+#include <kernel/mmu.h>
+#include <common/assert.h>
 
 #define PT_ENTRY    0b11
 #define PT_PAGE     0b11     
 #define PT_BLOCK    0b01    
 
-/* Lower Attributes */
-// bits[63:52, 11:2] are identical for both block and entry descriptors
-// level 0, and level 1 entries cannot be block descriptors
-// accessibility
-
-#define PT_EL0_RW   0b010
-
-
-#define PT_KERNEL   (0<<6)      // privileged, supervisor EL1 access only
-#define PT_USER     (1<<6)      // unprivileged, EL0 access allowed
-#define PT_RW       (0<<7)      // read-write
-#define PT_RO       (1<<7)      // read-only
-#define PT_AF       (1<<10)     // accessed flag
-#define PT_NX       (1UL<<54)   // no execute
-// shareability
-#define PT_OSH      (2<<8)      // outter shareable
-#define PT_ISH      (3<<8)      // inner shareable
-// indexes to be defined in MAIR register
-#define PT_MEM      (0<<2)      // normal memory
-#define PT_DEV      (1<<2)      // device MMIO
-#define PT_NC       (2<<2)      // non-cachable
-
-#define TTBR_CNP    1
+#define LEVEL1_BLOCKSIZE (1 << 21)
 
 #define PAGE_OFFSET_MASK (0xFFF)
 #define PAGE_MASK (~0xFFF)
 #define UPPER_BITS_MASK (0xFFFF000000000000) //Check the upper bit addr?
 
-// MAP MMIO block upper to lower half
-
-#define BLOCK_SIZE MB(2)
 #define ENTRIES_PER_PAGE 512
 
-#define PAGE_OFF 14
+#define PAGE_OFF 12
 #define PAGE_MASK BITS(PAGE_OFF)
-#define L3_BIT_OFF 12
-#define L2_BIT_OFF 21
-#define L1_BIT_OFF 30
-#define L0_BIT_OFF 39
-#define L3_MASK (BITS(9) << L3_BIT_OFF)
+
+#define L2_BIT_OFF 12
+#define L1_BIT_OFF 21
+#define L0_BIT_OFF 30
+
 #define L2_MASK (BITS(9) << L2_BIT_OFF)
 #define L1_MASK (BITS(9) << L1_BIT_OFF)
 #define L0_MASK (BITS(9) << L0_BIT_OFF)
-#define ADDR_MASK (BITS(48) & ~PAGE_MASK)
 
-static uint64_t high_l0_entry = 0;
-static uint64_t low_l0_entry = 0;
-static uint64_t *high_l1_table_start;
-static uint64_t *low_l1_table_start;
+#define L2_ENTRY(ADDR) ((((ADDR) & L2_MASK) >> L2_BIT_OFF) % ENTRIES_PER_PAGE)
+#define L1_ENTRY(ADDR) ((((ADDR) & L1_MASK) >> L1_BIT_OFF) % ENTRIES_PER_PAGE)
+#define L0_ENTRY(ADDR) (((ADDR) & L0_MASK) >> L0_BIT_OFF)
 
-#define MEM_LIMIT 32 //in GB
+#define PAGE_NORMALIZED_ADDR(ADDR) ((ADDR) / PAGE_SIZE)
+#define BLOCK_NORMALIED_ADDR(ADDR) ((ADDR) / LEVEL1_BLOCKSIZE)
+
+#define ADDR_MASK (BITS(39) & ~PAGE_MASK)
+
+static uint64_t *high_l0_entry;
+static uint64_t *low_l0_entry;
+
+static map_entry(uint64_t addr, uint64_t attrs)
+{
+    uint64_t entry_l1;
+    uint64_t entry_l0;
+    uint64_t * entry_l1_addr;
+    uint32_t l0_index;
+    uint32_t l1_index;
+
+    l0_index = L0_ENTRY(addr);
+
+    DEBUG_DATA("Mapping addr=", addr);
+
+    entry_l0 = low_l0_entry[l0_index];
+
+    if (entry_l0 == 0) {
+        entry_l1_addr = (uint64_t *)mm_earlypage_alloc(1);
+        memset(entry_l1_addr, 0, PAGE_SIZE);
+        low_l0_entry[l0_index] = PT_SECURE | (uint64_t)entry_l1_addr | PT_ENTRY;
+    }
+
+    entry_l1_addr = (uint64_t *)(entry_l0 & PT_ADDR);
+    l1_index = L1_ENTRY(addr);
+
+    DEBUG_DATA("entry_l1_addr=", entry_l1_addr);
+    DEBUG_DATA_DIGIT("l0_index=", l0_index);
+    DEBUG_DATA_DIGIT("l1_index=", l1_index);
+
+    entry_l1 = entry_l1_addr[l1_index];
+
+    if (entry_l1 == 0) {
+        entry_l1 |= attrs |
+                    PT_ACCESSED | 
+                    PT_BLOCK | 
+                    addr;
+
+        entry_l1_addr[l1_index] = entry_l1;
+    }
+}
 
 /* We will have a 38 Bit addressable Mem range = 512GB. 
  * Start L1 map contingous mem with 32GB Addressable mem. */
-
-static uint32_t table_size = 0;
-
-static int mmu_map_entry(uint64_t *phys_addr, uint64_t *virt_addr, uint64_t attributes)
-{   
-
-    uint64_t *l0_tbl = NULL;
-    uint64_t *l1_tbl = NULL;
-    uint64_t *l2_tbl = NULL;
-    uint64_t *l3_tbl = NULL;
-    uint64_t *virt_addr_normalized = (uint64_t*)((uint64_t)virt_addr & ~UPPER_BITS_MASK);
-    uint64_t *phys_addr_normalized = (uint64_t*)((uint64_t)phys_addr  & ~PAGE_MASK);
-
-    int PT_PRIV = 0;
-    /*
-    printf("MMU_MAP_ENTRY: phys =");
-    uart_hex(phys_addr);
-    printf(" virt_addr= ");
-    uart_hex(virt_addr);
-    printf("\n");
-    */
-
-    if  ((uint64_t)virt_addr_normalized & L0_MASK) {
-        printf("L0 MASK non zero above addressable range\n");
-        return 1;
-    }
-
-    if ((uint64_t)virt_addr & UPPER_BITS_MASK) {
-        l1_tbl = (uint64_t *)high_l1_table_start;
-        PT_PRIV = PT_KERNEL;
-    } else {
-        l1_tbl = (uint64_t *)low_l1_table_start;
-        PT_PRIV = PT_KERNEL;
-    }
-
-    ASSERT(l1_tbl);
-
-    uint64_t l1_table_off = (uint64_t)virt_addr_normalized >> L1_BIT_OFF;
-    uint64_t l2_table_off = (uint64_t)virt_addr_normalized >> L2_BIT_OFF;
-    uint64_t l3_table_off = (uint64_t)virt_addr_normalized >> L3_BIT_OFF;
-
-    int l2_tbl_idx = l2_table_off % ENTRIES_PER_PAGE;
-    int l3_tbl_idx = l3_table_off % ENTRIES_PER_PAGE;
-
-
-
-    /*
-    printf("l1_table_off= ");
-    uart_hex(l1_table_off);
-    printf(" l2_table_off= ");
-    uart_hex(l2_table_off);
-    printf(" l3_table_off= ");
-    uart_hex(l3_table_off);
-    printf("\n");
-    */
-
-    if (l1_table_off >= 32) {
-        printf("Addressing greater than 32GB\n");
-        return 1;
-    }
-
-     /* The L1 will point to the first 1GB block of memory */
-    if (l1_tbl[l1_table_off] == 0)  {
-        if ((l2_tbl = mm_earlypage_alloc(1)) == NULL) {
-            printf("l2_table alloc failed\n");
-            return 1;
-        }
-
-        l1_tbl[l1_table_off] = (uint64_t)l2_tbl |
-                                            PT_ENTRY |
-                                            PT_AF |
-                                            PT_PRIV|
-                                            PT_ISH |
-                                            PT_MEM;
-    }
-
-    l2_tbl = (uint64_t *) (l1_tbl[l1_table_off] & PAGE_MASK); //0 out upper bits?
-    
-    if (l2_tbl[l2_tbl_idx] == 0) {
-        /* This is a memory block descriptor of 2MB, */
-        if (attributes & PT_BLOCK) {
-            if(!IS_ALIGNED((uint64_t)virt_addr_normalized, MB(2))) {
-                printf("PT_BLOCK VIRT ADDR NOT ALIGNED\n");
-                ASSERT(0);
-                return 1;
-            }
-            
-            l2_tbl[l2_tbl_idx] = (uint64_t)phys_addr_normalized |
-                                            PT_BLOCK |
-                                            PT_PRIV |
-                                            attributes;
-            return 0;
-        }
-
-        if ((l3_tbl = mm_earlypage_alloc(1)) == NULL) {
-            printf("l3_table alloc failed\n");
-            return 1;
-        }
-
-        l2_tbl[l2_tbl_idx] = (uint64_t)l3_tbl |
-                                            PT_ENTRY |
-                                            PT_AF |
-                                            PT_PRIV |
-                                            PT_ISH |
-                                            PT_MEM;
-    }
-
-    l3_tbl = (uint64_t *) (l2_tbl[l2_tbl_idx] & PAGE_MASK);
-    /* The L3 table entries might not be contiguous since we will alloc
-     * them on the fly. */
-    if (l3_tbl[l3_tbl_idx] == 0)  {
-        l3_tbl[l3_tbl_idx] = (uint64_t)phys_addr_normalized |
-                                                    PT_PAGE |
-                                                    PT_PRIV |
-                                                    attributes;
-                                                    
-    }
-    
-   // printf("VIRT ADDR ");
-   // uart_hex((uint64_t) virt_addr);
-   // printf("\n");
-   // printf("PHYS ADDR ");
-   // uart_hex((uint64_t) phys_addr);
-   // printf("\n");
-
-
-    ASSERT(l1_tbl && l2_tbl  && l3_tbl);
-
-    return 0;
-}
-
-
-int mmu_init()
+int mmu_init(uint32_t phy_mem_size, uint32_t vc_mem_start, uint32_t vc_mem_size)
 {
-    uint64_t r;
+    uint32_t vc_block_size = vc_mem_size / LEVEL1_BLOCKSIZE;
     uint64_t attr = 0;
-    uint64_t *addr = NULL;
+    uint64_t addr = 0;
+    uint64_t reg = 0;
+    uint64_t r = 0;
+    uint64_t id = 0;
 
-    printf("MMU INIT ENTER\n");
-    /* 32 GB addr space for high and low mem. */
-    high_l1_table_start = mm_earlypage_alloc(1);
-    low_l1_table_start = mm_earlypage_alloc(1);
+    uint64_t * low_l1_entry;
 
-    printf("==MMU INIT==\n");
-    printf("high_l1 addr: ");
-    uart_hex(high_l1_table_start);
-    printf("\n");
+    low_l0_entry = (uint64_t *)mm_earlypage_alloc(1);
+    low_l1_entry = (uint64_t *)mm_earlypage_alloc(1);
+    memset(low_l0_entry, 0, PAGE_SIZE);
+    memset(low_l1_entry, 0, PAGE_SIZE);
 
-    printf("low_l1 addr: ");
-    uart_hex(low_l1_table_start);
-    printf("\n");
-
-
-    for (int i = BLOCK_SIZE; i < MB(64); i+= BLOCK_SIZE) {
-         addr = (uint64_t *)i;
+    low_l0_entry[0] = PT_SECURE | (uint64_t)low_l1_entry | PT_ENTRY;
+    
+    uint64_t vc_ram_start = vc_mem_start / LEVEL1_BLOCKSIZE;
+    for (uint64_t i = 0; i < vc_ram_start; i++) {
+        map_entry(i * LEVEL1_BLOCKSIZE, PT_MEM_ATTR(MT_NORMAL) | PT_INNER_SHAREABLE);
     }
 
-
-    for (int i = 0; i < MB(4); i += PAGE_SIZE) {
-        attr = 0;
-        addr = (uint64_t *)i;
-
-        attr = PT_AF | PT_NX;
-        if  (i >= MMIO_BASE && i <= MMIO_BASE + PAGE_SIZE) {
-            attr |= PT_OSH | PT_DEV;
-           // printf("Hit MMIOU ADDR: ");
-           // uart_hex(i);
-          //  printf("\n");
-        } else {
-            attr |= PT_ISH | PT_MEM;
-        }
-
-        r = mmu_map_entry(addr, addr, attr);
-        if (r != 0) {
-             printf("Map fail on virt addr");
-            uart_hex(addr);
-            printf("\n");
-            ASSERT(0);
-            return 1;
-        } 
-        
-
-        r = mmu_map_entry(addr, ((uint64_t)addr) + UPPER_ADDR_MASK, attr);
-        if (r != 0) {
-            printf("Map fail on UPPER virt addr");
-            uart_hex(((uint64_t)addr) + UPPER_ADDR_MASK);
-            printf("\n");
-            ASSERT(0);
-            return 1;
-        } 
-        
+    uint64_t vc_ram_end = (MMIO_BASE) / LEVEL1_BLOCKSIZE;
+    for (uint64_t i = vc_ram_start; i < vc_ram_end; i++) {
+        map_entry(i * LEVEL1_BLOCKSIZE, PT_MEM_ATTR(MT_NORMAL_NC));
     }
 
-    //printf("MMIO BASE MAPPING\n");
-    attr = PT_AF | PT_NX | PT_OSH | PT_DEV | PT_PAGE;
-    addr = (uint64_t *)MMIO_BASE;
-    for (int i = 0; i < KB(16); i += PAGE_SIZE) {
-
-        r = mmu_map_entry(addr, addr, attr);
-        if (r != 0) {
-             printf("Map fail on virt addr");
-            uart_hex(addr);
-            printf("\n");
-            ASSERT(0);
-            return 1;
-        } 
-        
-
-        r = mmu_map_entry(addr, ((uint64_t)addr) + UPPER_ADDR_MASK, attr);
-        if (r != 0) {
-            printf("Map fail on UPPER virt addr");
-            uart_hex(((uint64_t)addr) + UPPER_ADDR_MASK);
-            printf("\n");
-            ASSERT(0);
-            return 1;
-        } 
-
-        addr += i;
-
+    uint64_t vc_mem_end = ((vc_mem_size - (MMIO_BASE - vc_mem_start)) + MMIO_BASE) / LEVEL1_BLOCKSIZE;
+    for (uint64_t i = vc_ram_end; i < vc_mem_end; i++) {
+        map_entry(i * LEVEL1_BLOCKSIZE, PT_MEM_ATTR(MT_DEVICE_NGNRNE));
     }
-
-    printf("MAPPING DONE\n");
-    r = 0;
-
-    asm volatile ("mrs %0, id_aa64mmfr0_el1" : "=r" (r));
-    uint64_t b = r & 0xF;
-    if (b < 1) {
-        printf("Atleast 36 bit phys addr size not supported\n");
-        ASSERT(0);
-        return 1;
-    }
-
+    
+    // Full system data barrier, make sure all writes are commited
+    aarch64_dsb();
     // first, set Memory Attributes array, indexed by PT_MEM, PT_DEV, PT_NC in our example
-    r=  (0xFF << 0) |    // AttrIdx=0: normal, IWBWA, OWBWA, NTR
-        (0x04 << 8) |    // AttrIdx=1: device, nGnRE (must be OSH too)
-        (0x44 <<16);     // AttrIdx=2: non cacheable
-    asm volatile ("msr mair_el1, %0" : : "r" (r));
-    printf("MAPPING 1\n");
-    r = 0;
-   // r = 0x80100100;
-    
-    // next, specify mapping characteristics in translate control register
-    r=  (0b00LL << 37) | // TBI=0, no tagging
-        (b << 32) |      // IPS=autodetected
-        (0b10LL << 30) | // TG1=4k
-        (0b11LL << 28) | // SH1=3 inner
-        (0b01LL << 26) | // ORGN1=1 write back
-        (0b01LL << 24) | // IRGN1=1 write back
-        (0b0LL  << 23) | // EPD1 enable higher half
-        (25LL   << 16) | // T1SZ=25, 3 levels (512G)
-        (0b00LL << 14) | // TG0=4k
-        (0b11LL << 12) | // SH0=3 inner
-        (0b01LL << 10) | // ORGN0=1 write back
-        (0b01LL << 8) |  // IRGN0=1 write back
-        (0b0LL  << 7) |  // EPD0 enable lower half
-        (25LL   << 0);   // T0SZ=25, 3 levels (512G)
-    
-    asm volatile ("msr tcr_el1, %0; isb" : : "r" (r));
-printf("MAPPING 2\n");
-       // tell the MMU where our translation tables are. TTBR_CNP bit not documented, but required
-    // lower half, user space
-    //asm volatile ("msr ttbr0_el1, %0" : : "r" (((uint64_t)low_l1_table_start) + 1));
-    asm volatile ("msr ttbr0_el1, %0" : : "r" ((uint64_t)low_l1_table_start));
-    // upper half, kernel space
-    //asm volatile ("msr ttbr1_el1, %0" : : "r" (((uint64_t)high_l1_table_start) + 1));
-     asm volatile ("msr ttbr1_el1, %0" : : "r" ((uint64_t)high_l1_table_start));
+    r = MAIR1_VAL;
 
+    AARCH64_MSR(mair_el1, r);
+
+    AARCH64_MSR(ttbr0_el1, (uint64_t)low_l0_entry);
+    AARCH64_MSR(ttbr1_el1, (uint64_t)low_l0_entry);
+
+    AARCH64_MRS(id_aa64mmfr0_el1, id);
+
+    aarch64_isb();
+    //DEBUG_DATA_DIGIT("mem model features=",id);
     
     r = 0;
-printf("MAPPING 3\n");
-    // finally, toggle some bits in system control register to enable page translation
-    asm volatile ("dsb ish; isb; mrs %0, sctlr_el1" : "=r" (r));
-   // r|=0xC00800;     // set mandatory reserved bits
-    r&=~((1<<25) |   // clear EE, little endian translation tables
-         (1<<24) |   // clear E0E
-         (1<<19) |   // clear WXN
-         (1<<12) |   // clear I, no instruction cache
-         (1<<4) |    // clear SA0
-         (1<<3) |    // clear SA
-         (1<<2) |    // clear C, no cache at all
-         (1<<1));    // clear A, no aligment check
-    r|=  (1<<0);     // set M, enable MMU
-    printf("OK\n");
-    asm volatile ("msr sctlr_el1, %0; isb" : : "r" (r));
+    r =  (0b00LL << 38) | // TBI1=0, Use top byte for address calculation for TTBR1_EL1
+        (0b00LL << 37) | // TBI0=0, Use top byte for address calculation for TTBR0_EL1
+        (0b0LL  << 36) | // AS, ASID size, 8 bit default
+        (id & 0b111 << 32) | // IPS size
+        (0b10LL << 30) | // TG1=4k granulatity
+        (0b11LL << 28) | // SH1=3 Sharability for inner mem only
+        (0b01LL << 26) | // ORGN1=1 same as below but for outer memory
+        (0b01LL << 24) | // IRGN1=1 write back write-allocate cacheable on page table inner mem for TTBR1_EL1
+        (0b0LL  << 23) | // EPD1, enable table walks on tlb miss for TTBR1_EL1
+        (0b0LL  << 22) | // A1, TTBR0_EL1 defines the ASID values
+        (25LL   << 16) | // T1SZ=25, 3 levels (512G) for ttbr1_el1
+        (0b00LL << 14) | // TG0=4k granularity
+        (0b11LL << 12) | // SH0=3 Sharability for inner mem only
+        (0b01LL << 10) | // ORGN0=1 same as below but for outer memory
+        (0b01LL << 8) |  // IRGN0=1 write back write-allocate cacheable on on page table inner memory
+        (0b0LL  << 7) |  // EPD0, enable table walks on TLB miss for TTBR0_EL1
+        (25LL   << 0);   // T0SZ=25, 3 levels (512G), for ttbr0_el1
+    
+    AARCH64_MSR(tcr_el1, r);
 
+    aarch64_isb();
 
-    printf("==MMU INIT OK==\n");
+    r = 0;
+    AARCH64_MRS(sctlr_el1, r);
+
+    r |= SCTLREL1_VAL;
+
+    AARCH64_MSR(sctlr_el1, r);
+ 
+    aarch64_isb();
+
+    DEBUG("--- MMU INIT DONE ---");
 
     return 0;
 }
+
+/*
+static int mmu_map_entry(uint64_t *phys_addr, uint64_t *virt_addr, uint64_t attributes)
+{
+
+} */
