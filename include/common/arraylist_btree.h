69325db827a20e8eeaeb696788e18ef2eb860c43
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2024-09-27 17:55:21 -0700

    common/arraylist_btree.h: Add explainer comment
    Explain the data structure a bit and why I am not using it for anything
    :-)
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/common/arraylist_btree.h
+++ include/common/arraylist_btree.h
@@ -5,6 +5,14 @@
 #include <common/common.h>
 #include <common/math.h>
 
+/* This is a data structure I wrote initially to be a fast(ish) lockless array btree bitmap
+ * that operates on small (63 entries, 5 tree levels) tree sizes.
+ * It was initially written for the memory manager but ran in to complications as the max levels were
+ * too small for the amount of memory sizes I wanted to keep track of. As well as a lot of the allocs would be page level
+ * and having the "small" allocator take from the pool led to some other complexities (that I don't remember now). 
+ * I also wanted to make a more straightforward buddy allocator like Linux.
+ * But I will leave here since why not, might be useful somewhere. */
+
 #define AL_BTREE_MAX_LEVEL 5 //Log2(64) - 1, with 0th level at root
 #define AL_BTREE_LEVEL_NUM (AL_BTREE_MAX_LEVEL + 1)
 #define AL_BTREE_NUM_ENTRIES 63
