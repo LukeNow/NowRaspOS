c6cd841df82eda45be2ca3cf5468b2fe419baad2
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2025-02-20 20:07:47 -0800

    common/atomic: Add lds and strs and move include references
    Add lds and strs for 64 and 32 bit variants.
    
    Move all includes to reference the asm file directly as having wrappers
    in a C file did not make any sense.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/common/atomic.h
+++ include/common/atomic.h
@@ -7,9 +7,20 @@
 #define ATOMIC_UINT64(X) __attribute__((aligned(8))) static uint64_t (X) = 0
 #define ATOMIC_UINT32(X) __attribute__((aligned(4))) static uint32_t (X) = 0
 
-int atomic_cmpxchg(uint64_t *ptr, uint64_t old_val, uint64_t new_val);
-int atomic_add(uint64_t *addr, uint64_t val);
-int atomic_sub(uint64_t *addr, uint64_t val);
-int atomic_or(uint64_t *addr, uint64_t val);
+typedef uint32_t atomic32_t;
+typedef uint64_t atomic64_t;
+
+extern int atomic_cmpxchg_64(uint64_t *ptr, uint64_t old_val, uint64_t new_val);
+/* Try the cmpxchg a number of times. */
+int atomic_cmpxchg_try_64(uint64_t * ptr, uint64_t old_val, uint64_t new_val, uint32_t tries);
+/* Eventually for these atomic ops we can eventually do it how linux does it and
+ * do some pattern substitution with the ops since most of the assembly is the same. */
+extern uint64_t atomic_fetch_add_64(uint64_t *addr, uint64_t val);
+extern uint64_t atomic_fetch_sub_64(uint64_t *addr, uint64_t val);
+extern uint64_t atomic_fetch_or_64(uint64_t *addr, uint64_t val);
+extern uint32_t atomic_ld_32(uint32_t * ptr);
+extern uint64_t atomic_ld_64(uint64_t * ptr);
+extern int atomic_str_32(uint32_t * ptr, uint32_t val);
+extern int atomic_str_64(uint64_t * ptr, uint64_t val);
 
 #endif
--- src/common/atomic.c
+++ src/common/atomic.c
@@ -1,30 +1,17 @@
 #include  <stddef.h>
 #include  <stdint.h>
 
-extern int _atomic_cmpxchg(uint64_t *ptr, uint64_t old_val, uint64_t new_val);
-int atomic_cmpxchg(uint64_t *ptr, uint64_t old_val, uint64_t new_val)
+int atomic_cmpxchg_try_64(uint64_t * ptr, uint64_t old_val, uint64_t new_val, uint32_t tries)
 {
-	return _atomic_cmpxchg(ptr, old_val, new_val);
-}
+	int ret;
 
-// For these atomic ops we can do how linux does it
-// and dynamically pass instructions we want to be atomic
-// since the pattern is the same for these simple ops
+	while (tries) {
+		ret = atomic_cmpxchg_64(ptr, old_val, new_val);
+		if (!ret)
+			return ret;
+		
+		tries--;
+	}
 
-extern int _atomic_fetch_add(uint64_t *ptr, uint64_t val);
-int atomic_add(uint64_t *ptr, uint64_t val)
-{
-	return _atomic_fetch_add(ptr, val);
-}
-
-extern int _atomic_fetch_sub(uint64_t *ptr, uint64_t val);
-int atomic_sub(uint64_t *ptr, uint64_t val)
-{
-	return _atomic_fetch_sub(ptr, val);
-}
-
-extern int _atomic_fetch_or(uint64_t *ptr, uint64_t val);
-int atomic_or(uint64_t *ptr, uint64_t val)
-{
-	return _atomic_fetch_or(ptr, val);
+	return ret;
 }
