c18221dfdc87342332a90cef5ff3f1b2cdb1eba9
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2025-03-02 16:06:53 -0800

    kernel/irq: Add initial sync_exception handler
    Add the initial implementation for the syncronous expection handler for
    software interrupts.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/kernel/irq.h
+++ include/kernel/irq.h
@@ -4,6 +4,7 @@
 void irq_init();
 void handle_invalid_irq(int irq_entry);
 void handle_irq();
+extern void irq_sync_el1(uint64_t vector);
 extern void irq_vector_init();
 extern void irq_enable();
 extern void irq_disable();
--- src/kernel/irq.c
+++ src/kernel/irq.c
@@ -7,6 +7,9 @@
 #include <common/assert.h>
 #include <common/common.h>
 #include <kernel/timer.h>
+#include <common/aarch64_common.h>
+#include <common/bits.h>
+#include <kernel/cpu.h>
 
 void irq_init() 
 {
@@ -24,6 +27,21 @@ void handle_invalid_irq(int irq_entry)
     uart_hex(irq_entry);
     printf("\n");
     return;
+}
+
+void handle_sync_irq(uint64_t exception)
+{
+    uint64_t esr;
+    DEBUG_DATA("SYNC HANDLER CALLED =", exception);
+
+    DEBUG_DATA("CPU ID=", cpu_get_id());
+    AARCH64_MRS(esr_el1, esr);
+
+    DEBUG_DATA("Code1=", esr);
+    DEBUG_DATA_BINARY("Code1=", (uint32_t)esr);
+    DEBUG_DATA_BINARY("Code2=", (uint32_t)esr>>32);
+
+    DEBUG_PANIC("STOP");
 }
 
 void handle_irq()
