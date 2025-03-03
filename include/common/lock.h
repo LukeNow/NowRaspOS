0232653e28116ae5800275abe2142afef5f92adf
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2025-03-02 16:12:07 -0800

    common/lock: Implement spinlocks with irqsaves
    As well as add proper memory barriers in functions needed to ensure
    correctness for smp.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- include/common/lock.h
+++ include/common/lock.h
@@ -10,13 +10,13 @@ typedef lock_t spinlock_t;
 #define DEFINE_LOCK(X) __attribute__((aligned(8))) static lock_t (X) = 0
 #define DEFINE_SPINLOCK(X) __attribute__((aligned(8))) static spinlock_t (X) = 0
 
-int lock_init(lock_t *lock);
-int spinlock_init(spinlock_t *lock);
+int lock_init(lock_t * lock);
+int spinlock_init(spinlock_t * lock);
+int sleeplock_init(sleeplock_t * lock);
+int lock_trylock(lock_t * lock);
+int lock_tryunlock(lock_t * lock);
+void spinlock_irqsave(lock_t * lock, uint64_t * flags);
+void spinunlock_irqrestore(lock_t * lock, uint64_t flags);
 
-int lock_trylock(lock_t *lock);
-int lock_tryunlock(lock_t *lock);
-
-void lock_spinlock(spinlock_t *lock); 
-void lock_spinunlock(spinlock_t *lock);
 
 #endif
--- src/common/lock.c
+++ src/common/lock.c
@@ -1,38 +1,69 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <kernel/irq.h>
+#include <common/aarch64_common.h>
 #include <common/lock.h>
 #include <common/atomic.h>
 #include <common/common.h>
 
-int lock_init(lock_t *lock)
+int lock_init(lock_t * lock)
 {
     *lock = 0;
+
+    return 0;
 }
 
-int spinlock_init(spinlock_t *lock)
+int spinlock_init(spinlock_t * lock)
 {
     *lock = 0;
+
+    return 0;
 }
 
-int lock_trylock(lock_t *lock)
+int sleeplock_init(sleeplock_t * lock)
+{
+    spinlock_init(&lock->list_lock);
+    ll_head_init(&lock->sleep_list, LIST_NODE_T);
+    lock->lock = 0;
+
+    return 0;
+}
+
+int lock_trylock(lock_t * lock)
 {
     /* 0 for success in lock, 1 for lock is currently taken or there is contention. */
-    return atomic_cmpxchg(lock, 0, 1);
+    return atomic_cmpxchg_64(lock, 0, 1);
 }
 
-int lock_tryunlock(lock_t *lock)
+int lock_tryunlock(lock_t * lock)
 {
+    aarch64_dmb();
     *lock = 0;
 }
 
-void lock_spinlock(lock_t *lock)
+void lock_spinlock(lock_t * lock)
 {
     while (lock_trylock(lock)) {
         CYCLE_WAIT(5);
     }
 }
 
-void lock_spinunlock(lock_t  *lock)
+void spinlock_irqsave(lock_t * lock, uint64_t * flags)
 {
+    int ret;
+
+    irq_save_disable(flags);
+    lock_spinlock(lock) ;  
+}
+
+void spinunlock_irqrestore(lock_t * lock, uint64_t flags)
+{
+    lock_tryunlock(lock);
+    irq_restore(flags);
+}
+
+void lock_spinunlock(lock_t  * lock)
+{
+    aarch64_dmb();
     *lock = 0;
 }
