530e44f6ae8a9acc661cf55d37a7e7dc7a0caf72
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2025-03-11 19:06:32 -0700

    common/atomic.c: Set ret to 0
    Set it to zero because incase the given tries are 0.
    
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- src/common/atomic.c
+++ src/common/atomic.c
@@ -1,9 +1,10 @@
-#include  <stddef.h>
-#include  <stdint.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <common/atomic.h>
 
 int atomic_cmpxchg_try_64(uint64_t * ptr, uint64_t old_val, uint64_t new_val, uint32_t tries)
 {
-	int ret;
+	int ret = 0;
 
 	while (tries) {
 		ret = atomic_cmpxchg_64(ptr, old_val, new_val);
