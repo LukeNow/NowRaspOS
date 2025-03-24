59adfb25059f66544319d610c0813bef6ba5ddb2
Luke Nowakowski-Krijger <lukenowakow@proton.me>
2025-03-23 17:58:38 -0700

    common/lock: Implement semaphore and rw_lock
    Signed-off-by: Luke Nowakowski-Krijger <lukenowakow@proton.me>

--- src/common/lock.c
+++ src/common/lock.c
@@ -62,3 +62,60 @@ void unlock_spinlock(lock_t  * lock)
     aarch64_dmb();
     *lock = 0;
 }
+
+void init_sempahore(semaphore_t * sem, unsigned int max_num)
+{
+    sem->max_num = max_num;
+    sem->num = max_num;
+}
+
+void semaphore_take(semaphore_t * sem)
+{
+    atomic_semaphore_take(&sem->num);
+}
+
+void semaphore_give(semaphore_t * sem)
+{
+    atomic_semaphore_give(&sem->num);
+}
+
+void semaphore_set(semaphore_t * sem, unsigned int expected, unsigned int new_val)
+{
+    int ret;
+    do {
+        ret = atomic_cmpxchg_64(&sem->num, expected, new_val);
+    } while (ret);
+}
+
+/* Max_reader_count can be set to MAX_INT if you want unlimited reads. */
+void rwlock_init(rw_lock_t * lock, unsigned int max_reader_count)
+{
+    lock->max_reader_count = max_reader_count;
+    lock->writer_waiting = 0;
+    init_sempahore(&lock->sem, max_reader_count);
+}
+
+void rwlock_read_lock(rw_lock_t * lock)
+{
+    while (atomic_ld_64(&lock->writer_waiting)) {}
+    semaphore_take(&lock->sem);
+}
+
+void rwlock_write_lock(rw_lock_t * lock)
+{
+    atomic_fetch_add_64(&lock->writer_waiting, 1);
+    semaphore_set(&lock->sem, lock->max_reader_count, 0);
+}
+
+void rwlock_read_unlock(rw_lock_t * lock)
+{
+    semaphore_give(&lock->sem);
+    aarch64_dmb();
+}
+
+void rwlock_write_unlock(rw_lock_t * lock)
+{
+    semaphore_set(&lock->sem, 0, lock->max_reader_count);
+    atomic_fetch_sub_64(&lock->writer_waiting, 1);
+    aarch64_dmb();
+}
